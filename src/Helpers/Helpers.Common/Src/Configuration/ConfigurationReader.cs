f546a2a24c2c42b7b5b2fbf2c6aeddfea647fc41
LeonidK <LeonidK@brokeree.com>
2020-06-29 00:14:25 +0300

    Add common tools project

---
+++ src/Helpers/Helpers.Common/Src/Configuration/ConfigurationReader.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace Helpers.Common.Src.Configuration
+{
+    public static class ConfigurationReader
+    {
+        public static T ReadConfig<T>(string fileName = "config.json") where T : new()
+        {
+            if (File.Exists(fileName))
+            {
+                var text = File.ReadAllText(fileName);
+                return JsonConvert.DeserializeObject<T>(text);
+            }
+            else
+            {
+                var defaultJson = JsonConvert.SerializeObject(new T(), Formatting.Indented);
+                File.WriteAllText(fileName, defaultJson);
+                var defaultConfig = File.ReadAllText(fileName);
+                return JsonConvert.DeserializeObject<T>(defaultConfig);
+            }
+        }
+    }
+}
